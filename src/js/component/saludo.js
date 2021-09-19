5365d090ae1f1654fdd78590a23cbd855672658e
alonatt <alondra08@ug.uchile.cl>
2021-09-19 21:34:21 +0000

    masavnce

--- src/js/component/saludo.js
+++ src/js/component/saludo.js
@@ -9,27 +9,15 @@ export const Saludo = () => {
 		<>
 			{store.usuarios.map((persona, posicion) => {
 				return (
-					<div className="container row mx-1 justify-content-center" key={posicion}>
-						{" "}
-						<div className="card mx-1">
-							<div className="row col-1">
-								<span> {persona.id} </span>
-							</div>
-							<img
-								src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Retrato_del_Maestro_Yoda.jpg/245px-Retrato_del_Maestro_Yoda.jpg"
-								className="card-img-top"
-								alt="..."
-							/>
-							<div className="card-body">
-								<h5 className="card-title">{persona.name}</h5>
-								<div className="row">
-									<div className="col col-6">
-										<Link to={"/persona/" + persona.id}>
-											<button className="btn btn-secondary"> Ir a perfil</button>
-										</Link>
-									</div>
-								</div>
-							</div>
+					<div key={posicion}>
+						<div className="alert alert-secondary" role="alert" key={posicion}>
+							<span> {persona.id} </span> <h2>Bienvenido {persona.name}</h2>
+						</div>
+
+						<div className="col col-6">
+							<Link to={"/persona/" + persona.id}>
+								<button className="btn btn-secondary"> Ir a perfil</button>
+							</Link>
 						</div>
 					</div>
 				);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import { Saludo } from "../component/saludo";
 
 export const Home = () => (
 	<div className="text-center mt-5">
 		<h1>Hello Rigo!</h1>
+		<Saludo />
 		<p>
 			<img src={rigoImage} />
 		</p>
