eb75d551b18d3e7333772593328ba1ab5a3b8896
alonatt <alondra08@ug.uchile.cl>
2021-09-20 02:25:00 +0000

    vistavendedor

--- src/js/views/vendedor.js
+++ src/js/views/vendedor.js
@@ -1,25 +1,23 @@
 import React from "react";
+import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 import { Saludo } from "../component/saludo";
-import { Context } from "../store/appContext";
 
-export const Vendedor = () => {
-	<>
-		<div className="container-fluid row">
-			<div className="col col-2" />
-			<div className="col col-10 text-center mt-5">
-				<div clasName="row">
-					<h1>Bienvenido </h1>
-					<h2>
-						<Saludo />
-					</h2>
+export const Vendedor = () => (
+	<div className="container-fluid row">
+		<div className="col col-2" />
+		<div className="col col-10 text-center mt-5">
+			<div clasName="row">
+				<h1>Bienvenido </h1>
+				<h2>
+					<Saludo />
+				</h2>
 
-					<p />
-					<a href="#" className="btn btn-success">
-						If you see this green button, bootstrap is working
-					</a>
-				</div>
+				<p />
+				<a href="#" className="btn btn-success">
+					If you see this green button, bootstrap is working
+				</a>
 			</div>
 		</div>
-	</>;
-};
+	</div>
+);
